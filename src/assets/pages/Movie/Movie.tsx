aa5d269fdcb595653b75769ba035f61103a7aac7
Jakub Mielewczyk <jkbmielewczyk@gmail.com>
2023-02-11 19:51:32 +0100

    Adding more to movie page.

--- src/assets/pages/Movie/Movie.tsx
+++ src/assets/pages/Movie/Movie.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import { AiOutlinePlayCircle } from 'react-icons/ai';
 import { BsDot, BsFillArrowRightCircleFill } from 'react-icons/bs';
 import { Link, useParams } from 'react-router-dom';
-import { getAllGenres } from '../../utils/fetchFunctions';
 import { getPosters, getVideos } from '../../utils/MovieFunctions';
+import { FaRegSadCry } from 'react-icons/fa';
 
 const Movie = () => {
   const [title, setTitle] = useState<string>('');
@@ -79,9 +79,9 @@ const Movie = () => {
 
   const windowWidth = window.innerWidth - 50;
   const windowHeight = windowWidth * (3 / 4);
-  console.log(genres);
+  console.log(cast);
   return (
-    <div className="flex flex-col w-screen min-h-screen overflow-hidden pl-5 pr-5 bg-black gap-10">
+    <div className="flex flex-col w-screen min-h-screen overflow-hidden pl-5 pr-5 bg-black gap-7">
       <header className="w-full h-1/4 relative overflow-hidden rounded-lg ">
         <div className="absolute h-full w-48 from-black via-black bg-gradient-to-r"></div>
         <div className="absolute h-full flex justify-start items-center">
@@ -93,88 +93,140 @@ const Movie = () => {
           <img className="ml-2.5 rounded-lg" src={headerImage}></img>
         )}
       </header>
-      <div className="w-full  ">
-        <h1 className="text-white text-center text-3xl">{`${title} (${releaseYear})`}</h1>
-      </div>
-      <div className="flex gap-2.5">
-        <div className="w-full">
-          <p className="text-white">Community Score: {voteAverage}/10</p>
+      <main className="flex flex-col w-full gap-6">
+        <div className="w-full  ">
+          <h1 className="text-white text-center text-3xl">{`${title} (${releaseYear})`}</h1>
         </div>
-        <div className="w-full pl-10">
-          <Link to={movieHomepage}>
-            <button className="text-xl bg-white w-full h-full rounded-lg flex justify-center items-center gap-2.5">
-              Watch <AiOutlinePlayCircle className="text-xl" />
-            </button>
-          </Link>
-        </div>
-      </div>
-      <div className="flex items-center justify-center gap-1">
-        <span className="text-white">{fullRelease}</span>
-        <BsDot className="text-white" />
-        <span className="text-white">{runtime}</span>
-        {genres &&
-          genres.map((element: { name?: string }) => (
-            <span className="text-white">{element.name}</span>
-          ))}
-      </div>
-      <main className="w-full">
-        <div className="flex flex-col w-full gap-5">
-          <div className="flex flex-col w-full">
-            {videos !== null && videos.length > 0 ? (
-              <>
-                <p className="text-white text-3xl ">Videos</p>
-                <div className="flex gap-5 w-full overflow-x-scroll transition-all">
-                  {videos.map((object) => (
-                    <div className="flex justify-center items-center w-full flex-shrink-0">
-                      <iframe
-                        className="w-full"
-                        width={`${windowWidth}`}
-                        height={`${windowHeight}`}
-                        src={`https://www.youtube.com/embed/${object.key}`}
-                      ></iframe>
-                    </div>
-                  ))}
-                  {loadMoreVideosBtn && (
-                    <div className="flex justify-center items-center">
-                      <BsFillArrowRightCircleFill
-                        onClick={() => {
-                          let maxNumber = maxVideosVisible;
-                          maxNumber += 5;
-                          setMaxVideosVisible(maxNumber);
-                        }}
-                        className="text-white text-5xl"
-                      />
-                    </div>
-                  )}
-                </div>
-              </>
-            ) : null}
+        <div className="flex gap-2.5">
+          <div className="w-full">
+            <p className="text-white">Community Score: {voteAverage}/10</p>
           </div>
-          <p className="text-white text-3xl ">Posters</p>
-          <div className="flex overflow-x-scroll">
-            {images &&
-              images.map((object) => (
-                <div className="w-44 max-h-[250px] flex-shrink-0 pr-3 overflow-hidden">
-                  <img
-                    className="rounded-lg object-fill"
-                    src={`${urlForImage}${object.file_path}`}
-                    alt=""
-                  />
-                </div>
-              ))}
-            {loadMorePostersBtn && (
-              <div className="flex justify-center items-center">
-                <BsFillArrowRightCircleFill
-                  onClick={() => {
-                    let maxNumber = maxPostersVisible;
-                    maxNumber += 20;
-                    setMaxPostersVisible(maxNumber);
-                  }}
-                  className="text-white text-5xl"
+          <div className="w-full pl-10">
+            <Link to={movieHomepage}>
+              <button className="text-xl bg-white w-full h-full rounded-lg flex justify-center items-center gap-2.5">
+                Watch <AiOutlinePlayCircle className="text-xl" />
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-wrap items-center justify-center gap-1 border-2">
+          <span className="text-white">{fullRelease}</span>
+          <BsDot className="text-white" />
+          <span className="text-white">{runtime}</span>
+          <div className="w-full text-center">
+            {genres &&
+              genres.map((element: { name?: string }, index: number) => {
+                if (index === genres.length - 1) {
+                  return <span className="text-white">{element.name}</span>;
+                } else {
+                  return <span className="text-white">{element.name}, </span>;
+                }
+              })}
+          </div>
+        </div>
+        <p className="text-slate-400 italic">{tagline}</p>
+        <p className="text-3xl text-white">Description</p>
+        <p className="text-white">{overview}</p>
+        {director &&
+          director.map((object: { original_name?: string; job?: string }) => {
+            return (
+              <div>
+                <p className="text-white font-bold">{object.original_name}</p>
+                <p className="text-white font-light ">{object.job}</p>
+              </div>
+            );
+          })}
+        <p className="text-3xl text-white">Cast</p>
+        <div className="flex w-full overflow-x-scroll">
+          {cast &&
+            cast.map(
+              (object: {
+                profile_path?: string | null;
+                name?: string;
+                character?: string;
+              }) => {
+                return (
+                  <div className="flex flex-col justify-between w-44 border-2  flex-shrink-0 mr-3 rounded-lg">
+                    {typeof object.profile_path === 'string' ? (
+                      <img
+                        className="object-cover"
+                        src={`${urlForImage}${object.profile_path}`}
+                        alt=""
+                      />
+                    ) : (
+                      <div className="flex flex-col justify-center gap-5 items-center w-full h-3/4 mb-2">
+                        <p className="text-white text-center">
+                          Sorry, no image for this person
+                        </p>
+                        <FaRegSadCry className="text-white text-5xl" />
+                      </div>
+                    )}
+                    <div className="w-full flex flex-grow flex-col justify-center items-center">
+                      <p className="text-white font-bold">{object.name}</p>
+                      <p className="text-white font-light">
+                        {object.character}
+                      </p>
+                    </div>
+                  </div>
+                );
+              }
+            )}
+        </div>
+        <div className="flex flex-col w-full">
+          {videos !== null && videos.length > 0 ? (
+            <>
+              <p className="text-white text-3xl ">Videos</p>
+              <div className="flex gap-5 w-full overflow-x-scroll transition-all">
+                {videos.map((object) => (
+                  <div className="flex justify-center items-center w-full flex-shrink-0">
+                    <iframe
+                      className="w-full"
+                      width={`${windowWidth}`}
+                      height={`${windowHeight}`}
+                      src={`https://www.youtube.com/embed/${object.key}`}
+                    ></iframe>
+                  </div>
+                ))}
+                {loadMoreVideosBtn && (
+                  <div className="flex justify-center items-center">
+                    <BsFillArrowRightCircleFill
+                      onClick={() => {
+                        let maxNumber = maxVideosVisible;
+                        maxNumber += 5;
+                        setMaxVideosVisible(maxNumber);
+                      }}
+                      className="text-white text-5xl"
+                    />
+                  </div>
+                )}
+              </div>
+            </>
+          ) : null}
+        </div>
+        <p className="text-white text-3xl ">Posters</p>
+        <div className="flex overflow-x-scroll">
+          {images &&
+            images.map((object) => (
+              <div className="w-44 max-h-[250px] flex-shrink-0 pr-3 overflow-hidden">
+                <img
+                  className="rounded-lg object-fill"
+                  src={`${urlForImage}${object.file_path}`}
+                  alt=""
                 />
               </div>
-            )}
-          </div>
+            ))}
+          {loadMorePostersBtn && (
+            <div className="flex justify-center items-center">
+              <BsFillArrowRightCircleFill
+                onClick={() => {
+                  let maxNumber = maxPostersVisible;
+                  maxNumber += 20;
+                  setMaxPostersVisible(maxNumber);
+                }}
+                className="text-white text-5xl"
+              />
+            </div>
+          )}
         </div>
       </main>
     </div>
